51001d35d300e4cd5d069e5e57d6b0d2d98608f2
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-21 21:19:07 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletofilesystem/SpringbootUploadMultipleFileToFileSystemApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootuploadmultiplefiletofilesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUploadMultipleFileToFileSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUploadMultipleFileToFileSystemApplication.class, args);
+    }
+}
