aa44a3ffc678ef8059b740740278a0c824305ece
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-21 21:21:20 +0700

    #3 - Add File Info Model

---
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletofilesystem/model/FileInfo.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootuploadmultiplefiletofilesystem.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-upload-multiple-file-to-file-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/11/18
+ * Time: 21.20
+ */
+
+@Data
+public class FileInfo {
+    private String filename;
+    private String url;
+}
