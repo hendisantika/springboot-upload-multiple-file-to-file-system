dea0bd090156d53c5f4e24cb32e8c3160055f8a3
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-21 21:27:24 +0700

    #9 - Modify Main App

--- src/main/java/com/hendisantika/springbootuploadmultiplefiletofilesystem/SpringbootUploadMultipleFileToFileSystemApplication.java
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletofilesystem/SpringbootUploadMultipleFileToFileSystemApplication.java
@@ -1,12 +1,25 @@
 package com.hendisantika.springbootuploadmultiplefiletofilesystem;
 
+import com.hendisantika.springbootuploadmultiplefiletofilesystem.service.FileStorage;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class SpringbootUploadMultipleFileToFileSystemApplication {
+public class SpringbootUploadMultipleFileToFileSystemApplication implements CommandLineRunner {
+
+    @Resource
+    FileStorage fileStorage;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootUploadMultipleFileToFileSystemApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        fileStorage.deleteAll();
+        fileStorage.init();
     }
 }
